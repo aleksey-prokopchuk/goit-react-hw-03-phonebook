3273a3334a5cc4229392fe6be86fe2abc41871ff
aleksey-prokopchuk <prokop4huk@ukr.net>
2022-09-20 21:08:27 +0300

    Завершив
    1 контакти зберігаються у локальне сховище.
    2. контакти, якщо такі є, зчитуються з локального сховища і записуються у стан

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -10,19 +10,22 @@ const { wrapper } = css;
 
 class Phonebook extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
-  isDuplicate({ name }) {
-    const { contacts } = this.state;
-    const result = contacts.find(item => item.name === name);
-    return result;
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
   }
 
   addContact = contact => {
@@ -34,6 +37,12 @@ class Phonebook extends Component {
       return { contacts: [newContact, ...prevState.contacts] };
     });
   };
+
+  isDuplicate({ name }) {
+    const { contacts } = this.state;
+    const result = contacts.find(item => item.name === name);
+    return result;
+  }
 
   removeContact = id => {
     this.setState(prevState => {
@@ -63,7 +72,6 @@ class Phonebook extends Component {
       const result =
         normalisedName.includes(normalisedFilter) ||
         number.includes(normalisedFilter);
-      console.log(result);
       return result;
     });
     return filteredContacts;
